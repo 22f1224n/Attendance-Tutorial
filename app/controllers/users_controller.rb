e41c462d01f28c3855051f552e91c4c5bd4c4495
f,saitou <22f12v3v24n@gmail.com>
2020-02-08 14:50:20 +0000

    Finish editing-users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-    
-  def show
-    @user = User.find(params[:id])
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate(page: params[:page])
   end
-  
+
+  def show
+  end
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -21,30 +25,39 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
   def edit
-    @user = User.find(params[:id])
   end
-  
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
     else
-      render :edit
+      render :edit      
     end
   end
-  
+
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-    
+
+    # beforeフィルター
+
+    # paramsハッシュからユーザーを取得します。
     def set_user
       @user = User.find(params[:id])
     end
 
+    # ログイン済みのユーザーか確認します。
     def logged_in_user
       unless logged_in?
         store_location
@@ -52,8 +65,14 @@ class UsersController < ApplicationController
         redirect_to login_url
       end
     end
-    
+
+    # アクセスしたユーザーが現在ログインしているユーザーか確認します。
     def correct_user
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # システム管理権限所有かどうか判定します。
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
