bfe1a83ae9a073d01f825a39b9e879b972641a64
22f1224n <22f12v3v24n@gmail.com>
2020-08-02 01:58:17 +0000

    8/1

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,9 +1,17 @@
 module AttendancesHelper
 
-  def attendance_state(attendance)
+  def attendance_work(attendance)
     # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
       return '出勤' if attendance.started_at.nil?
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    false
+  end
+  
+  def attendance_leaving_work(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
       return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
     # どれにも当てはまらなかった場合はfalseを返します。
